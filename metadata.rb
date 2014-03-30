7acfd2a79be59afd3ccdcc55d54a2a895ca2d60b
Sean Porter <portertech@gmail.com>
2014-03-30 13:40:25 -0700

    version bump, manage multiple contacts

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "support@hw-ops.com"
 license          "MIT"
 description      "Installs/Configures flapjack"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version          "0.3.0"
+version          "0.4.0"
 
 depends "ruby_installer"
 depends "redisio"
